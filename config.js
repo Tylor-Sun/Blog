6f10cf6d628a403845b2113fe52db47584732d1a
Tylor-Sun <1259848890@qq.com>
2019-08-23 18:25:08 +0800

    PersonalBlog

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ var conf = fs.readFileSync(path.join(__dirname, "/server.conf"));
 var confArr = conf.toString().split("\r\n");
 
 for(var i = 0; i < confArr.length ; i++){
+    
     globalConfig[confArr[i].split("=")[0]] = confArr[i].split("=")[1];
 }
 
--- utils/createTimeUtil.js
+++ utils/createTimeUtil.js
@@ -1,5 +1,4 @@
 function getNowTime(){
     return parseInt(Date.now() / 1000);
 }
-
 module.exports.getNowTime = getNowTime;
